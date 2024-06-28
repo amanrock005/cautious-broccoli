cf2e1f6d76e3b69bcdbeae899bb1262d3463b4f6
amanrock005 <46340802+amanrock005@users.noreply.github.com>
2024-06-28 13:33:19 +0530

    Create toepliz.py

---
+++ toepliz.py
@@ -0,0 +1,8 @@
+def toepliz_matrx(mat):
+    rows = len(mat)
+    cols = len(mat[0])
+    for i in range (1,rows):
+        for j in range (1,cols):
+            if (mat[i][j]!=mat[i-1][j-1]):
+                return False
+    return True
